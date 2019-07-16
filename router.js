97149629982f165a7e3de1339451ec854e8dbe42
Nourr <hadrich.nour@gmail.com>
2019-07-16 12:27:12 +0100

    first commit

---
+++ router.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+
+/*local*/
+
+const user = require('./controllers/userController');
+const todo = require('./controllers/todoController');
+const crud = require('./controllers/crud');
+const models = require('./models/models');
+
+
+
+
+
+var app = express();
+var port = "3000";
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+//app.use('/api/jokes', require(crud)(models.user));
+
+
+
+
+app.use("/user",user);
+app.use("/todo",todo);
+
+
+app.get("/user",(req,res)=>{
+    res.send({
+        message : "<h1>welcome to the server</h1>"
+    });
+});
+
+
+app.get("/todo",(req,res)=>{
+    res.send({
+        message : "<h1>welcome to to-do controller</h1>"
+    });
+});
+
+
+// Server
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+
+
+
+
